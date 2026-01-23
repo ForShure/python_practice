1e6e6983dd6491b2b201ce1af7744e84760a9ef7
ForShure <evildevilzp@gmail.com>
2026-01-23 15:31:32 +0200

    Final version for deploy

--- render_start.sh
+++ render_start.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+# Добавляем папку web в список мест, где Python ищет файлы
+export PYTHONPATH=$PYTHONPATH:./web
+
 gunicorn web.web.wsgi:application --bind 0.0.0.0:$PORT &
 python main.py
