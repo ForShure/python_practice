0087c57ba22df6fd8b354a0f84d454f9cff9dc73
ForShure <evildevilzp@gmail.com>
2026-01-23 15:31:51 +0200

    Fix python path for shop module

--- render_start.sh
+++ render_start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 # Добавляем папку web в список мест, где Python ищет файлы
 export PYTHONPATH=$PYTHONPATH:./web
-
 gunicorn web.web.wsgi:application --bind 0.0.0.0:$PORT &
 python main.py
